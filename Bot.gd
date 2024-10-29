143e4fbb16d1497dbd3c0d7a9e5888403b75eb9c
LuK4s09 <lukasduerotin04@gmail.com>
2024-10-29 20:42:31 +0100

    Worked on Platform_physics
    Package now falling with linear speed
    Platform now has minimum y-position
    removed floor

--- Bot.gd
+++ Bot.gd
@@ -1,25 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 600.0
-const JUMP_VELOCITY = -400.0
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var SPEED = 800.0
+var min_y_position = 970
 
 func _physics_process(delta):
-	# Handle jump.
-	#if Input.is_action_pressed("ui_accept"):
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+	velocity.x = 0
 	
-
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= SPEED
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += SPEED
+	
 	move_and_slide()
+	
+	if position.y > min_y_position:
+		position.y = min_y_position
+
--- Package.gd
+++ Package.gd
@@ -1,12 +1,18 @@
 extends RigidBody2D
-const FALL_SPEED = 100.0
+var FAlL_SPEED = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	gravity_scale = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	set_sleeping(false)
+	
+	linear_velocity.y += FAlL_SPEED
+	
 
+#func _on_body_entered(body):
+#	if body.name == "Bot":
+#		gravity_scale = 0.1
