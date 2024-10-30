fd48f9e7919fc393f1a5df0de938ade925b0676b
deChilonio <168724847+oneandonlybasti@users.noreply.github.com>
2024-10-30 15:01:23 +0100

    Update Bot.gd

--- Bot.gd
+++ Bot.gd
@@ -1,31 +1,13 @@
 extends CharacterBody2D
 
 var JUMP_SPEED = 100
-var SPEED = 800.0
+var SPEED = 500.0
 var min_y_position = 970
 
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * SPEED
+
 func _physics_process(delta):
-	velocity.x = 0
-	velocity.y = 0
-	#Input for left & right movement
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= SPEED
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += SPEED
-	
-	
-	# min height
-	if position.y > min_y_position:
-		position.y = min_y_position
-	
-	#Jump-Ability 
-	if Input.is_action_pressed("ui_accept"):
-		velocity.y -= JUMP_SPEED
-	 
+	get_input()
 	move_and_slide()
-
-
-
-
-func _on_package_body_entered(body):
-	pass
