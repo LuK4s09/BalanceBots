fa36c872c428bbeef8d81148e5000e13d36377a0
Pulvr <pulv3rtoastmann@gmail.com>
2024-11-03 11:49:31 +0100

    Timer Spawner and Organisation
    Added A timer for Boxes to spawn, moved files around

---
+++ main/PackageSpawner.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@export var Package = preload("res://packages/package.tscn")
+
+func _on_timer_timeout():
+		#instance package scene
+	var package = Package.instantiate()
+	#set spawn position random
+	package.position = Vector2(randf_range(0, 1920),-100)
+	#add package to scene
+	get_parent().add_child(package)
