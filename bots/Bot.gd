6c55e087d51134d44a0c50197026ebbdfe3d96cb
Florian W <pulv3rtoastmann@gmail.com>
2024-11-04 15:28:36 +0100

    Added Bot Sprite and Animations for left and right

--- bots/Bot.gd
+++ bots/Bot.gd
@@ -1,12 +1,23 @@
 extends CharacterBody2D
 
+@onready var Bot1LookRight : AnimatedSprite2D = $Bot1LookRight
+@onready var Bot1LookLeft : AnimatedSprite2D = $Bot1LookLeft
+
 var SPEED = 650.0
 #var min_y_position = 970
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * SPEED
-
+	if velocity.x > 0 :
+		Bot1LookLeft.hide()
+		Bot1LookRight.show()
+		Bot1LookRight.play()
+	elif velocity.x < 0 : 
+		Bot1LookRight.hide()
+		Bot1LookLeft.show()
+		Bot1LookLeft.play()
+	
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
