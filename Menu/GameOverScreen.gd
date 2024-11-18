2060a054eb7d932c62214b74847395508114b484
LuK4s09 <lukasduerotin04@gmail.com>
2024-11-18 15:48:49 +0100

    Added TimerLabel & changed GameOverScreens Background to last frame of the game

--- Menu/GameOverScreen.gd
+++ Menu/GameOverScreen.gd
@@ -1,9 +1,12 @@
 extends Control
 
-
+@onready var background = $Background #reference to background node
+var last_frame: Texture = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#apply texture if it was set before scene ready
+	if last_frame:
+		apply_last_frame()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +16,16 @@ func _process(delta):
 
 func _on_button_pressed():
 		get_tree().change_scene_to_file("res://Menu/startscreen.tscn")
+		queue_free()
+		get_tree().paused = false
+
+func store_last_frame(texture):
+	last_frame = texture
+	if is_inside_tree(): #apply texture if scene ready
+		apply_last_frame()
+
+func apply_last_frame():
+	if background:
+		background.texture = last_frame
+	else:
+		print("ERROR: Background node is missing!")
